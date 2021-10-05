8b37538334a43069a1e37b63437b4d38255bd8aa
QuentinPotgieterBitBucket <quentinpotgieter@hotmail.co.za>
2021-10-05 19:18:46 +0200

    More Classes

---
+++ Classes/SalesRequest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NissanManufacturingPlanning.Classes
+{
+    class SalesRequest
+    {
+        private int quantity, derivativeId, planId;
+
+        public SalesRequest()
+        {
+            new SalesRequest(0, 0, 0);
+        }
+
+        public SalesRequest(int quantity, int derivativeId, int planId)
+        {
+            setDemand(quantity, derivativeId, planId);
+        }
+
+        private void setDemand(int quantity, int derivativeId, int planId)
+        {
+            this.quantity = quantity;
+            this.derivativeId = derivativeId;
+            this.planId = planId;
+        }
+
+        private int getQuantity()
+        {
+            return quantity;
+        }
+
+        private int getdDerivativeId()
+        {
+            return derivativeId;
+        }
+
+        private int getPlanId()
+        {
+            return planId;
+        }
+
+        public Boolean isPlanEqual(int capacity)
+        {
+            return quantity == capacity;
+        }
+
+    }
+}
---
+++ Classes/Vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NissanManufacturingPlanning.Classes
+{
+    class Vehicle
+    {
+        private String name;
+
+        protected Vehicle()
+        {
+            new Vehicle("");
+        }
+
+        protected Vehicle(String name)
+        {
+            this.name = name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+    }
+}
